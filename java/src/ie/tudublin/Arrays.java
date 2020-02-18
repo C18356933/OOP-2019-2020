564b5475b412e77ee01234c2b1fa78f5a3fc19bf
c18356933@mytudublin.ie <c18356933@mytudublin.ie>
2020-02-18 18:18:38 +0000

    Update

--- java/src/ie/tudublin/Arrays.java
+++ java/src/ie/tudublin/Arrays.java
@@ -1,98 +1,72 @@
 package ie.tudublin;
 
-// Hello from Jacob
-
 import processing.core.PApplet;
 
 public class Arrays extends PApplet
-{	
-
-	//float[] rainFall = new float[12]; 
-	float[] rainFall = {45, 37, 55, 27, 38, 50, 79, 48, 104, 31, 100, 58};
-	String[] months = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
+{
+	//float[] rainfall = new float[12];
+	float[] rainFall = {45,37,55,27,38,50,79,48,104,31,100,58};
+	String[] months = {"Jan", "Feb", "Mar", "Apr", "May", "Jun","Jul", "Aug","Sep", "Oct", "Nov", "Dec"};
 
 	public void settings()
 	{
 		size(500, 500);
 	}
 
-	public void setup() 
+	public void setup()
 	{
-		for(int i = 0 ; i < rainFall.length; i ++)
-		{
-			println(months[i] + "\t" + rainFall[i]);
-		}
+		int min = 0;
+		int max = 0;
 
-		for(float f:rainFall)
-		{
+		for (int i = 0; i < rainFall.length; i++) {
+			println(months[i] + "\t" + rainFall[i]);
+
+			if(rainFall[i]> rainFall[max]){
+				max = i;
+			}
+			if(rainFall[i]<rainFall[min]){
+				min = i;
+			}
+		}
+		println("Max is : " + months[max] + "\t" + rainFall[max]);
+		println("Min is : "+ months[min] + "\t" + rainFall[min]);
+		/*
+		for(float f:rainFall){
 			println(f);
 		}
-
-		for(String s:months)
-		{
-			println(s);
-		}
-
-		int minIndex = 0;
-		for(int i = 1 ; i < rainFall.length ; i ++)
-		{
-			if (rainFall[i] < rainFall[minIndex])
-			{
-				minIndex = i;
-			}
-		}
-		println(months[minIndex] + " had the minimum rainfall of " + rainFall[minIndex]);
-		
-		int maxIndex = 0;
-		for(int i = 1 ; i < rainFall.length ; i ++)
-		{
-			if (rainFall[i] > rainFall[maxIndex])
-			{
-				maxIndex = i;
-			}
-		}
-		println(months[maxIndex] + " had the maximum rainfall of " + rainFall[maxIndex]);
+		*/
 	}
 
-	void drawBarChart()
-	{
-		float w = width / (float) rainFall.length;
-		float cGap = 255 / (float) rainFall.length;
-		noStroke();
-		colorMode(HSB);
-		for(int i = 0 ; i < rainFall.length ; i ++)
-		{
-			float x = i * w;
-			fill(i * cGap, 255, 255);
-			rect(x, height, w, -rainFall[i]);
-		}
-	}
 
-	void test()
-	{
-		
-	}
-
-	
 
 	float offset = 0;
 
-	
-	
+
+
 	public void keyPressed()
 	{
 		if (key == ' ')
 		{
-			
+
 		}
-	}	
+	}
+
+	void drawbarchart(){
+		float w = width/(float) rainFall.length;
+		float cgap = 255/(float) rainFall.length;
+		for (int i = 0; i < rainFall.length; i++) {
+			float x = i*w;
+			fill(i*cgap, 255, 255);
+			rect(x,  height, w, -rainFall[i]); // tlx, tly, w, h
+		}
+
+	}
 
 
 	public void draw()
-	{	
-		background(0);		
-		colorMode(HSB);	
-
-		drawBarChart();
+	{
+		background(0);
+		colorMode(HSB);
+		drawbarchart();
 	}
 }
