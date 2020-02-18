8eb7fc48197242de885aa0947ae8a42e9bde47ea
c18356933@mytudublin.ie <c18356933@mytudublin.ie>
2020-02-18 18:01:02 +0000

    Tut finish

--- java/src/ie/tudublin/Loops.java
+++ java/src/ie/tudublin/Loops.java
@@ -3,93 +3,123 @@ package ie.tudublin;
 import processing.core.PApplet;
 
 public class Loops extends PApplet
-{	
+{
 	public void settings()
 	{
 		size(500, 500);
 	}
 
-	public void setup() 
+	public void setup()
 	{
-		
-	}
-
-	public void loops1()
-	{
-		stroke(255);
-		int numLines = (int) (30 * (mouseX / (float) width)) ;
-		float gap = width / (float) numLines;
-		for(int i = 0 ; i <= numLines ; i ++)
-		{
-			float x = i * gap;
-			line(x, 0, width - x, height);
-			line(0, x, width, height -x);
-		}
-	}
-
-	float offset = 0;
-
-	public void loops2()
-	{
-		int numCircles  = (int) (20 * (mouseX / (float) width)) ;
-		
-		float w = width / (float) numCircles;
-		float radius = w / 2.0f;
 		colorMode(HSB);
-		float cGap = 255 / (float) (numCircles * numCircles);
-		noStroke();
-
-		for(int j = 0 ; j < numCircles ; j ++)
-		{		
-			for(int i = 0 ; i < numCircles ; i ++)
-			{
-				float x = radius + (i * w);
-				float y = radius + (j * w);
-				float c = (cGap * i * j + offset) % 255; 
-				fill(c, 255, 255);
-				ellipse(x, y, w, w);
-			}
-		}
-		offset += mouseY / 250.0f;
 	}
 
-	public void loops3()
-	{
-		float gap = width * 0.1f;
-		float halfGap = gap / 2.0f;
-		colorMode(RGB);
-		stroke(0, 255, 0);
-		textAlign(CENTER, CENTER);
-		for(int i = -5 ; i <=5 ; i ++)
-		{
-			float x = map(i, -5, 5, gap, width -gap);				
-			line(x, gap, x, height - gap);
-			line(gap, x, width - gap, x);
-			fill(255);
-			text(i, x, halfGap);
-			text(i, halfGap, x);
-			
-		}
-	}
 
-	
 	public void keyPressed()
 	{
-		if (key == ' ')
-		{
-			
-		}
-	}	
+
+	}
 
 
 	public void draw()
-	{	
-		background(0);		
-		colorMode(HSB);
-		//loops1();
-
-		loops2();
-
-		//loops3();
+	{
+		//first();
+		//seconddraw();
+		//third();
+		//fourth();
+		fifth();
 	}
+
+	public void first(){
+		background(255);
+		int numlines = 100;
+		float gap = width/(float)numlines;
+		for ( int i = 0; i <= numlines; i++) {
+			stroke(0);
+			float x = i* gap;
+			line(x,0 ,width-x, height);
+			line(0,x ,width, height-x);
+
+		}
+	}
+
+	public void seconddraw(){
+		background(150);
+		int x =25;
+		int y = 250;
+		while ( x<500) {
+			stroke(0);
+			ellipse(x, y, 50, 50); // cx, cy, w, h
+
+			x= x +50;
+		}
+	}
+
+	public void third() {
+		background(150);
+		int x =0;
+		int y =0;
+		while ( x<500) {
+			stroke(0);
+			fill(y,10000 ,1000);
+			rect(x, 0, 50, 500); // tlx, tly, w, h
+			y +=20;
+			x += 50;
+		}
+	}
+
+	public void fourth() {
+		background(150);
+		int numcircles = 5;
+		int circlegap = width/(numcircles*2);
+		int x =circlegap;
+		int y = height/2;
+		int h = 0;
+		while ( x<width) {
+			stroke(0);
+			fill(h,255,255);
+			ellipse(x, y, circlegap, circlegap); // cx, cy, w, h
+			h +=20;
+			x += circlegap;
+		}
+	}
+
+	public void fifth() {
+		/*int x =25;
+		int y = 25;
+		int i = -5;
+		background(0);
+		stroke(124,10000,1000);
+		while ( y<height) {
+			line(25,y,475,y);
+			line(x,25,x,475);
+
+
+			textSize(10);
+			text(i, x, 15);
+			text(i, 10, y);
+			fill(124, 10000, 1000);
+			i++;
+			x += 45;
+			y += 45;*/
+
+			float gap = width * 0.1f;
+			float halfgap = gap/2.0f;
+			background(0);
+			colorMode(RGB);
+			stroke(0,255,0);
+			textAlign(CENTER, CENTER);
+			for (int i = -5; i < 5; i++) {
+				stroke(0,255,0);
+				float x = map(i,-5,5,gap,width-gap);
+				line(x, gap, x, height- gap);
+				line(gap, x, width-gap, x);
+				fill(255);
+				text(i,x,halfgap);
+				text(i, halfgap, x);
+			}
+
+
+		}
+
 }
